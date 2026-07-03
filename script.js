f351f0fbc65865c0e5c19cf3c5ee452b09cbb8ca
Vaibhav Prakash <vaibhavprakash549@gmail.com>
2026-07-03 23:54:19 +0530

    fix: Updated username

--- script.js
+++ script.js
@@ -188,7 +188,7 @@
     },
     'contact': function() {
       return '<span class="cm">email   </span>: vaibhav.prk05@gmail.com<br>'
-        + '<span class="cm">github  </span>: github.com/vaibhav-prka<br>'
+        + '<span class="cm">github  </span>: github.com/vaibhav-prk<br>'
         + '<span class="cm">linkedin</span>: linkedin.com/in/vaibhav-prakash-634a952ba<br>'
         + '<span class="cm">phone   </span>: +91 91995 96081';
     },
